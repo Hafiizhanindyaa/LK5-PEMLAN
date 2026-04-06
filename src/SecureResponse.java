9ada6ec1922677cfee48af20c9555f8b5de92010
MuhammadHafizhRaihan <hafizhb9@gmail.com>
2026-04-06 13:24:45 +0700

    Memperbaiki kelas PatientProfileV1 dan SecureResponse

--- src/SecureResponse.java
+++ src/SecureResponse.java
@@ -2,30 +2,40 @@ public class SecureResponse<T extends MedicalRecord & Confidential> {
 
     private boolean success;
     private T data;
-    private String warningMessage;  // Peringatan jika ada data yang di-masking
+    private String warningMessage;
+    private boolean dataMasked;
 
-    public SecureResponse(boolean success, T data, String warningMessage) {
-        this.success = success;
+    public SecureResponse(T data) {
+        this.success = true;
+        this.data = data;
+        this.warningMessage = null;
+        this.dataMasked = false;
+    }
+
+    public SecureResponse(T data, String warningMessage) {
+        this.success = true;
         this.data = data;
         this.warningMessage = warningMessage;
+        this.dataMasked = true;
     }
 
-    public boolean isSuccess() {
-        return success;
+    public SecureResponse(String errorMessage) {
+        this.success = false;
+        this.data = null;
+        this.warningMessage = errorMessage;
+        this.dataMasked = false;
     }
 
-    public T getData() {
-        return data;
-    }
-
-    public String getWarningMessage() {
-        return warningMessage;
-    }
+    public boolean isSuccess()          { return success; }
+    public T getData()                  { return data; }
+    public String getWarningMessage()   { return warningMessage; }
+    public boolean isDataMasked()       { return dataMasked; }
 
     @Override
     public String toString() {
         return "\n=== SecureResponse ===" +
                "\nStatus  : " + (success ? "SUCCESS" : "FAILED") +
+               "\nMasked  : " + (dataMasked ? "YA - sebagian data disembunyikan" : "TIDAK") +
                "\nWarning : " + (warningMessage != null ? warningMessage : "None") +
                "\nData    :\n       " + data;
     }
