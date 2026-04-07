b5d64f09e7f0e8f93af8e451022a296f462feca3
nfdhlarkn <nfdhlarkn@student.ub.ac.id>
2026-04-07 13:37:58 +0700

    Menambahkan file PatientProfileV2

---
+++ src/PatientProfileV2.java
@@ -0,0 +1,81 @@
+public class PatientProfileV2 implements MedicalRecord, Versioned, Confidential {
+
+    private String patientId;
+    private String name;
+    private String ssn;          
+    private int securityLevel;
+    private String diagnosis = "None";
+    private String medicalHistory = "None";
+
+    public PatientProfileV2(String patientId, String name, String ssn, int securityLevel, String diagnosis) {
+        this.patientId = patientId;
+        this.name = name;
+        this.ssn = ssn;
+        this.securityLevel = securityLevel;
+        this.diagnosis = diagnosis;
+    }
+    public PatientProfileV2(String patientId, String name, String ssn, String medicalHistory, int securityLevel) {
+        this.patientId = patientId;
+        this.name = name;
+        this.ssn = ssn;
+        this.securityLevel = securityLevel;
+        this.medicalHistory = medicalHistory;
+    }
+    public PatientProfileV2(String patientId, String name, String ssn, int securityLevel,String diagnosis, String medicalHistory) {
+        this.patientId = patientId;
+        this.name = name;
+        this.ssn = ssn;
+        this.securityLevel = securityLevel;
+        this.diagnosis = diagnosis;
+        this.medicalHistory = medicalHistory;
+    }
+
+
+    @Override
+    public String getPatientId() {
+        return patientId;
+    }
+
+    @Override
+    public int getVersion() {
+        return 1;
+    }
+
+    @Override
+    public int getSecurityLevel() {
+        return securityLevel;
+    }
+
+    @Override
+    public void maskSensitiveData() {
+        if (this.ssn != null) {
+            this.ssn = "******";  
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public String getMedicalHistory() {
+        return medicalHistory;
+    }
+
+    @Override
+    public String toString() {
+        return "[V1] Patient ID : " + patientId +
+               "\n       Name            : " + name +
+               "\n       KTP (SSN)       : " + ssn +
+               "\n       Security        : " + securityLevel +
+               "\n       Diagnosis       : " + diagnosis +
+               "\n       Medical History : " + medicalHistory;
+    }
+}
